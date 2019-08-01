d3883d6731b0c731fd3affa5d107491fbefb22b2
Kyrylo Hrecheniuk <kyryloh@wix.com>
2019-08-01 17:36:05 +0300

    initial commit

--- serverless/template-cms-snapshooter/serverless.js
+++ serverless/template-cms-snapshooter/serverless.js
@@ -1,5 +1,7 @@
+const {FullHttpResponse} = require('@wix/serverless-api');
+
 module.exports = functionsBuilder =>
   functionsBuilder
     .addWebFunction('GET', '/hello', async () => {
-      return { hello: 'world' };
+      return new FullHttpResponse({status: 204, body: {msg: 'Hello, World!'}});
     });
